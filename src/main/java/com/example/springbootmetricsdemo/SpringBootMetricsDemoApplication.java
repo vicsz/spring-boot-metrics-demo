17724ce5b0b34c7cd0728aaaaf74cbbd424d2d34
VicSz <vicsz>
2018-05-31 15:23:41 -0400

    Initial commit

---
+++ src/main/java/com/example/springbootmetricsdemo/SpringBootMetricsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootmetricsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMetricsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMetricsDemoApplication.class, args);
+	}
+}
