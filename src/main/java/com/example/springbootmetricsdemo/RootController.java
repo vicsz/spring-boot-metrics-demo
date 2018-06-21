c0d354148f1689b9c8432eeed0b1dc7d5b0ce505
VicSz <vicsz>
2018-06-21 15:59:12 -0400

    Cleanup GUI.

---
+++ src/main/java/com/example/springbootmetricsdemo/RootController.java
@@ -0,0 +1,25 @@
+package com.example.springbootmetricsdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class RootController {
+
+    @Value("${vcap.application.name:local_app}")
+    private String applicationName;
+
+    @Value("${vcap.application.space.name:local_space}")
+    private String spaceName;
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("applicationName", applicationName);
+        model.put("spaceName", spaceName);
+        return "index";
+    }
+
+}
