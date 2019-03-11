54c47d3ffb47663caeb933544ccfbb776c200747
VicSz <vicsz>
2019-03-11 11:19:35 -0400

    Display additonal information about PCF -- to make it easier to demo what happens when crash happens.

--- src/main/java/com/example/springbootmetricsdemo/RootController.java
+++ src/main/java/com/example/springbootmetricsdemo/RootController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.lang.management.ManagementFactory;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class RootController {
@@ -18,14 +20,32 @@ public class RootController {
     @Value("${vcap.application.instance_id:localInstanceId}")
     private String instanceId;
 
+    @Value("${vcap.application.instance_index:localInstanceIndex}")
+    private String instanceIndex;
+
     @RequestMapping("/")
     public String welcome(Map<String, Object> model) {
 
         model.put("applicationName", applicationName);
         model.put("spaceName", spaceName);
         model.put("instanceId", instanceId);
+        model.put("instanceIndex", instanceIndex);
+        model.put("jvmUpTime", getJvmUpTime());
 
         return "index";
+    }
+
+    private String getJvmUpTime() {
+
+        long uptimeInMilliSec = ManagementFactory.getRuntimeMXBean().getUptime();
+
+        long seconds = (uptimeInMilliSec / 1000) % 60;
+        long minutes = (uptimeInMilliSec / (1000 * 60)) % 60;
+        long hours = (uptimeInMilliSec / (1000 * 60 * 60)) % 24;
+        long days = (uptimeInMilliSec / (1000 * 60 * 60 * 24));
+
+        return "Days " + days + " Hours " + hours + " Minutes " + minutes + " Seconds " + seconds;
+
     }
 
 }
